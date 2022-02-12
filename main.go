4ddaca28b8ec8aa6d6b27b23a6506c77e4019e2d
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-02-12 09:13:13 +0200

    create http server

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"github.com/oleksiivelychko/go-microservice/handlers"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	l := log.New(os.Stdout, "go-microservice", log.LstdFlags)
+
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/", handlers.NewProducts(l))
+
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	sig := <-signalChannel
+	log.Println("Received terminate, graceful shutdown", sig)
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(ctx)
+}

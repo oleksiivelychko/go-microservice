2062be40e5823a882ec48b3ed73fde47629de9c8
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-03-13 06:46:18 +0200

    handlers struct

--- handlers/products.go
+++ handlers/products.go
@@ -1,90 +1,13 @@
 package handlers
 
 import (
-	"context"
-	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/oleksiivelychko/go-microservice/api"
+	"github.com/oleksiivelychko/go-microservice/helpers"
 	"log"
-	"net/http"
-	"strconv"
 )
 
 type Products struct {
 	l *log.Logger
-}
-
-func NewProducts(l *log.Logger) *Products {
-	return &Products{l}
+	v *helpers.Validation
 }
 
 type KeyProduct struct{}
-
-// swagger:route GET /products products listProducts
-// Returns a list of products from data store
-// responses:
-//	200: ProductsResponse
-func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle GET Products", r.URL.Path)
-
-	lp := api.GetProducts()
-	err := lp.ToJSON(w)
-	if err != nil {
-		http.Error(w, "Unable to marshal json", http.StatusInternalServerError)
-	}
-}
-
-func (p *Products) AddProduct(w http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle POST Products", r.URL.Path)
-
-	product := r.Context().Value(KeyProduct{}).(api.Product)
-	api.AddProducts(&product)
-}
-
-func (p *Products) UpdateProduct(w http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle PUT Products", r.URL.Path)
-
-	vars := mux.Vars(r)
-	id, err := strconv.Atoi(vars["id"])
-	if err != nil {
-		http.Error(w, "Unable to convert ID", http.StatusBadRequest)
-		return
-	}
-
-	product := r.Context().Value(KeyProduct{}).(api.Product)
-	err = api.UpdateProducts(id, &product)
-	if err == api.ErrProductNotFound {
-		http.Error(w, "Product not found", http.StatusNotFound)
-		return
-	}
-
-	if err != nil {
-		http.Error(w, "Product not found", http.StatusInternalServerError)
-		return
-	}
-}
-
-func (p *Products) MiddlewareProductValidation(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		product := api.Product{}
-
-		err := product.FromJSON(r.Body)
-		if err != nil {
-			p.l.Println("[ERROR] deserializing product", err)
-			http.Error(rw, "Error reading product", http.StatusBadRequest)
-			return
-		}
-
-		err = product.Validate()
-		if err != nil {
-			p.l.Println("[ERROR] validating product", err)
-			http.Error(rw, fmt.Sprintf("Error validating product: %s", err), http.StatusBadRequest)
-			return
-		}
-
-		ctx := context.WithValue(r.Context(), KeyProduct{}, product)
-		r = r.WithContext(ctx)
-
-		next.ServeHTTP(rw, r)
-	})
-}

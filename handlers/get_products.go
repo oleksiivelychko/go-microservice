9052b6192b0628511ec762eb1afdb07256224b64
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-03-13 11:02:08 +0200

    define get product(s) handler

---
+++ handlers/get_products.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"github.com/oleksiivelychko/go-microservice/api"
+	"github.com/oleksiivelychko/go-microservice/utils"
+	"net/http"
+)
+
+// swagger:route GET /products
+// Returns a list of products.
+// responses:
+//	200: productsResponse
+func (p *ProductHandler) GetAll(rw http.ResponseWriter, r *http.Request) {
+	p.L.Println("[DEBUG] handle GET:all products", r.URL.Path)
+
+	list := api.GetProducts()
+	err := utils.ToJSON(list, rw)
+	if err != nil {
+		p.L.Println("[ERROR] serializing product", err)
+	}
+}
+
+// swagger:route GET /products/{id}
+// Returns a single product by ID
+// responses:
+//	200: productResponse
+//  404: errorResponse
+func (p *ProductHandler) GetOne(rw http.ResponseWriter, r *http.Request) {
+	id := getProductID(r)
+	p.L.Println("[DEBUG] get record id", id)
+
+	product, err := api.GetProduct(id)
+
+	switch err {
+	case nil:
+	case api.ErrProductNotFound:
+		p.L.Println("[ERROR] fetching product", err)
+
+		rw.WriteHeader(http.StatusNotFound)
+		_ = utils.ToJSON(&GenericError{Message: err.Error()}, rw)
+		return
+	default:
+		p.L.Println("[ERROR] fetching product", err)
+
+		rw.WriteHeader(http.StatusInternalServerError)
+		_ = utils.ToJSON(&GenericError{Message: err.Error()}, rw)
+		return
+	}
+
+	err = utils.ToJSON(product, rw)
+
+	if err != nil {
+		p.L.Println("[ERROR] serializing product", err)
+	}
+}

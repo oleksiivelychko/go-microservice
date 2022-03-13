dc6dd0985de1b531bef5956184ff1e55b258c110
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-03-13 06:37:40 +0200

    add swagger route

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/go-openapi/runtime/middleware"
 	"github.com/gorilla/mux"
 	"github.com/oleksiivelychko/go-microservice/handlers"
 	"log"
@@ -19,12 +20,22 @@ func main() {
 
 	getRouter := serveMux.Methods(http.MethodGet).Subrouter()
 	getRouter.HandleFunc("/products", h.GetProducts)
+
 	postRouter := serveMux.Methods(http.MethodPost).Subrouter()
 	postRouter.HandleFunc("/products", h.AddProduct)
 	postRouter.Use(h.MiddlewareProductValidation)
+
 	putRouter := serveMux.Methods(http.MethodPut).Subrouter()
 	putRouter.HandleFunc("/products/{id:[0-9]+}", h.UpdateProduct)
 	putRouter.Use(h.MiddlewareProductValidation)
+
+	deleteRouter := serveMux.Methods(http.MethodDelete).Subrouter()
+	deleteRouter.HandleFunc("/products/{id:[0-9]+}", h.DeleteProduct)
+
+	opts := middleware.RedocOpts{SpecURL: "/swagger.yaml"}
+	apiHandler := middleware.Redoc(opts, nil)
+	getRouter.Handle("/docs", apiHandler)
+	getRouter.Handle("/swagger.yaml", http.FileServer(http.Dir("./")))
 
 	server := &http.Server{
 		Addr:         ":9090",
@@ -51,5 +62,5 @@ func main() {
 	log.Println("Received terminate, graceful shutdown", sig)
 
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	server.Shutdown(ctx)
+	_ = server.Shutdown(ctx)
 }

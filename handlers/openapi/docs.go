d4b475f082d08873fb5fae66308ef4ced3e399d3
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-03-13 08:40:36 +0200

    describe openapi documentation

---
+++ handlers/openapi/docs.go
@@ -0,0 +1,72 @@
+// Documentation of Product API
+//
+// 		Schemes: http
+// 		BasePath: /
+//		Version: 1.0.0
+//
+// 		Consumes:
+// 		- application/json
+// 		Produces:
+// 		- application/json
+//
+// swagger:meta
+package openapi
+
+import (
+	"github.com/oleksiivelychko/go-microservice/api"
+	"github.com/oleksiivelychko/go-microservice/handlers"
+)
+
+// NOTE: Types defined here are purely for documentation purposes these types are not used by any of the handlers
+
+// Generic error message returned as a string.
+// swagger:response errorResponse
+type errResponseWrapper struct {
+	// Description of the error
+	// in: body
+	Body handlers.GenericError
+}
+
+// Validation errors defined as an array of strings.
+// swagger:response errorValidation
+type errValidationWrapper struct {
+	// Collection of the errors
+	// in: body
+	Body handlers.ValidationError
+}
+
+// swagger:response productsResponse
+type productsResponseWrapper struct {
+	// A list of all products
+	// in: body
+	Body []api.Product
+}
+
+// Data structure representing a single product
+// swagger:response productResponse
+type productResponseWrapper struct {
+	// Newly created product
+	// in: body
+	Body api.Product
+}
+
+// No content is returned by this API endpoint
+// swagger:response noContentResponse
+type noContentResponseWrapper struct{}
+
+// swagger:parameters updateProduct createProduct
+type productParamsWrapper struct {
+	// Product data structure to Update or Create.
+	// Note: the id field is ignored by update and create operations
+	// in: body
+	// required: true
+	Body api.Product
+}
+
+// swagger:parameters updateProduct
+type productIDParamWrapper struct {
+	// The id of the product for which the operation relates
+	// in: path
+	// required: true
+	ID int `json:"id"`
+}

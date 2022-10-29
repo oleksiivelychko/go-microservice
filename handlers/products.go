cd68872cdca96bff557ce40354a357c5ba0b4a50
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-10-29 13:43:25 +0300

    inject product service inside handler

--- handlers/products.go
+++ handlers/products.go
@@ -2,29 +2,29 @@ package handlers
 
 import (
 	"github.com/gorilla/mux"
-	gService "github.com/oleksiivelychko/go-grpc-protobuf/proto/grpc_service"
+	"github.com/hashicorp/go-hclog"
+	"github.com/oleksiivelychko/go-microservice/service"
 	"github.com/oleksiivelychko/go-microservice/utils"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-// ProductHandler for getting and updating products
+// ProductHandler for getting and updating products.
 type ProductHandler struct {
-	l  *log.Logger
+	l  hclog.Logger
 	v  *utils.Validation
-	cc gService.CurrencyClient
+	ps *service.ProductService
 }
 
-// KeyProduct is a key used for the Product object in the context
+// KeyProduct is a key used for the Product object in the context.
 type KeyProduct struct{}
 
-// NewProductHandler returns a new product handler injected with logger, validation and gRPC client
-func NewProductHandler(l *log.Logger, v *utils.Validation, cc gService.CurrencyClient) *ProductHandler {
-	return &ProductHandler{l, v, cc}
+// NewProductHandler returns a new product handler injected with logger, validation and gRPC client.
+func NewProductHandler(l hclog.Logger, v *utils.Validation, ps *service.ProductService) *ProductHandler {
+	return &ProductHandler{l, v, ps}
 }
 
-// GenericError is a generic error message returned by a server
+// GenericError is a generic error message returned by a server.
 type GenericError struct {
 	Message string `json:"message"`
 }
@@ -39,7 +39,12 @@ type NotFound struct {
 	Message string `json:"message"`
 }
 
-// getProductID returns the product ID from the URL
+// GrpcError means that request to gRPC service is failed.
+type GrpcError struct {
+	Message string `json:"message"`
+}
+
+// getProductID returns the product ID from the URL.
 func getProductID(r *http.Request) int {
 	// parse the product id from the url
 	vars := mux.Vars(r)

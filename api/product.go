ceb0e8965763bbd36656e0dac8e712092c1dc366
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-10-29 10:18:14 +0300

    remove methods into service

--- api/product.go
+++ api/product.go
@@ -1,13 +1,7 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"strings"
-	"time"
+	"github.com/oleksiivelychko/go-microservice/utils"
 )
 
 // Product defines the structure for an API model.
@@ -29,7 +23,7 @@ type Product struct {
 	//
 	// required: true
 	// min: 0.01
-	Price float32 `json:"price" validate:"gt=0"`
+	Price float64 `json:"price" validate:"gt=0"`
 
 	// SKU - in the field of inventory management, a stock keeping unit is a distinct type of item for sale, purchased, or tracked in inventory,
 	// such as a product or service, and all attributes associated with the item type that distinguish it from other item types.
@@ -45,116 +39,7 @@ type Product struct {
 	// pattern: [a-z]+-[a-z]+-[a-z]+
 	SKU string `json:"sku" validate:"required,sku"`
 
-	UpdatedAt DateTime `json:"updatedAt,omitempty"`
-}
-
-type DateTime struct {
-	time.Time
-}
-
-func (t *DateTime) MarshalJSON() ([]byte, error) {
-	stamp := time.Now().Format(time.RFC3339)
-	return []byte("\"" + stamp + "\""), nil
-}
-
-func (t *DateTime) UnmarshalJSON(b []byte) (err error) {
-	s := strings.Trim(string(b), "\"")
-	date, err := time.Parse(time.RFC3339, s)
-	if err != nil {
-		return err
-	}
-	t.Time = date
-	return
+	UpdatedAt utils.DateTime `json:"updatedAt,omitempty"`
 }
 
 type Products []*Product
-
-var ErrProductNotFound = fmt.Errorf("product not found")
-
-var productsList = LoadProductsFromJSON()
-
-func GetProducts() Products {
-	return productsList
-}
-
-func GetProduct(id int) (*Product, error) {
-	i := findIndexByProductID(id)
-	if i == -1 {
-		return nil, ErrProductNotFound
-	}
-
-	return productsList[i], nil
-}
-
-func AddProduct(p *Product) {
-	p.ID = GetNextProductId()
-	productsList = append(productsList, p)
-}
-
-func UpdateProduct(p *Product) error {
-	i := findIndexByProductID(p.ID)
-	if i == -1 {
-		return ErrProductNotFound
-	}
-
-	productsList[i] = p
-	return nil
-}
-
-func DeleteProduct(id int) error {
-	i := findIndexByProductID(id)
-	if i == -1 {
-		return ErrProductNotFound
-	}
-
-	productsList = RemoveProductByIndex(productsList, i)
-	return nil
-}
-
-/*
-*
-findIndexByProductID finds the index of a product.
-Returns -1 when no product not found.
-*/
-func findIndexByProductID(id int) int {
-	for i, p := range productsList {
-		if p.ID == id {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func RemoveProductByIndex(s []*Product, index int) []*Product {
-	return append(s[:index], s[index+1:]...)
-}
-
-func GetNextProductId() int {
-	if len(productsList) == 0 {
-		return 1
-	}
-
-	return productsList[len(productsList)-1].ID + 1
-}
-
-func LoadProductsFromJSON() []*Product {
-	wd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	jsonFile, err := os.Open(filepath.Join(wd, "./public/products.json"))
-	if err != nil {
-		panic(err)
-	}
-
-	defer jsonFile.Close()
-
-	byteValue, _ := io.ReadAll(jsonFile)
-
-	var products Products
-	_ = json.Unmarshal(byteValue, &products)
-
-	return products
-}

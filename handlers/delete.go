48e0c56fc764d3ce9a485c91dd3e4300d8e7b7b6
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-10-29 13:52:43 +0300

    delete handler refactoring

--- handlers/delete.go
+++ handlers/delete.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"github.com/oleksiivelychko/go-microservice/api"
 	"github.com/oleksiivelychko/go-microservice/utils"
 	"net/http"
 )
@@ -12,17 +11,16 @@ import (
 // responses:
 // 204: noContentResponse
 // 404: notFoundResponse
-func (p *ProductHandler) DeleteProduct(rw http.ResponseWriter, r *http.Request) {
+func (ph *ProductHandler) DeleteProduct(rw http.ResponseWriter, r *http.Request) {
+	ph.l.Debug("DeleteProduct")
 	rw.Header().Add("Content-Type", "application/json")
 
 	id := getProductID(r)
-	p.l.Printf("[DEBUG] DELETE `/products/%d`", id)
 
-	err := api.DeleteProduct(id)
-	if err == api.ErrProductNotFound {
-		p.l.Printf("[ERROR] DELETE `/products/%d` got '%s'", id, err)
+	err := ph.ps.DeleteProduct(id)
+	if err != nil {
+		ph.l.Debug("product not found", "id", id)
 		rw.WriteHeader(http.StatusNotFound)
-
 		_ = utils.ToJSON(&NotFound{Message: err.Error()}, rw)
 		return
 	}

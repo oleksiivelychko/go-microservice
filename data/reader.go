12dd365a1308ed817b91dc8c32cd07bfe140a4ef
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-04 14:27:37 +0200

    check that file exists

--- data/reader.go
+++ data/reader.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/oleksiivelychko/go-microservice/api"
 	"io"
 	"os"
@@ -12,6 +14,10 @@ func LoadDataFromJson(jsonFilename string) ([]byte, error) {
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
+	}
+
+	if _, err = os.Stat(jsonFilename); errors.Is(err, os.ErrNotExist) {
+		return nil, errors.New(fmt.Sprintf("file %s does not exist", jsonFilename))
 	}
 
 	jsonFile, err := os.Open(filepath.Join(wd, jsonFilename))

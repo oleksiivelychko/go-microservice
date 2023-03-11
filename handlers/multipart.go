aa5961d6472f960d7d03e15d4732a4f8a586c6c7
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-11 21:06:05 +0200

    refactoring: rename variables

--- handlers/multipart.go
+++ handlers/multipart.go
@@ -5,7 +5,7 @@ import (
 	"github.com/oleksiivelychko/go-microservice/api"
 	"github.com/oleksiivelychko/go-microservice/contracts"
 	"github.com/oleksiivelychko/go-microservice/service"
-	"github.com/oleksiivelychko/go-microservice/utils"
+	validatorHelper "github.com/oleksiivelychko/go-utils/validator_helper"
 	"io"
 	"net/http"
 	"path/filepath"
@@ -14,76 +14,76 @@ import (
 
 // MultipartHandler for CRUD actions regarding api.Product objects as multipart/form-data.
 type MultipartHandler struct {
-	log hclog.Logger
-	val *utils.Validation
-	stg contracts.Storage
-	srv *service.ProductService
+	logger         hclog.Logger
+	validation     *validatorHelper.Validation
+	storage        contracts.Storage
+	productService *service.ProductService
 }
 
-func NewMultipartHandler(l hclog.Logger, v *utils.Validation, s contracts.Storage, ps *service.ProductService) *MultipartHandler {
+func NewMultipartHandler(l hclog.Logger, v *validatorHelper.Validation, s contracts.Storage, ps *service.ProductService) *MultipartHandler {
 	return &MultipartHandler{l, v, s, ps}
 }
 
-func (mp *MultipartHandler) ProcessForm(rw http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(128 * 1024) // 32Mb
+func (handler *MultipartHandler) ProcessForm(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseMultipartForm(128 * 1024) // 32Mb
 	if err != nil {
-		mp.log.Error("expected multipart form data", "error", err)
-		http.Error(rw, "expected multipart form data", http.StatusUnprocessableEntity)
+		handler.logger.Error("expected multipart form data", "error", err)
+		http.Error(writer, "expected multipart form data", http.StatusUnprocessableEntity)
 		return
 	}
 
-	id := r.FormValue("id")
+	id := request.FormValue("id")
 	productId, err := strconv.Atoi(id)
 	if err != nil {
-		productId = mp.srv.GetNextProductId()
+		productId = handler.productService.GetNextProductId()
 	}
 
-	price, err := strconv.ParseFloat(r.FormValue("price"), 64)
+	price, err := strconv.ParseFloat(request.FormValue("price"), 64)
 	if err != nil {
-		mp.log.Error("unable to parse price value to float type", "error", err)
-		http.Error(rw, "unable to parse price value to float type", http.StatusUnprocessableEntity)
+		handler.logger.Error("unable to parse price value to float type", "error", err)
+		http.Error(writer, "unable to parse price value to float type", http.StatusUnprocessableEntity)
 		return
 	}
 
 	product := api.Product{
 		ID:    productId,
-		Name:  r.FormValue("name"),
+		Name:  request.FormValue("name"),
 		Price: price,
-		SKU:   r.FormValue("SKU"),
+		SKU:   request.FormValue("SKU"),
 	}
 
-	imageFile, fileHeader, err := r.FormFile("image")
+	imageFile, fileHeader, err := request.FormFile("image")
 	if err != nil {
-		mp.log.Error("expected file", "error", err)
-		http.Error(rw, "expected file", http.StatusUnprocessableEntity)
+		handler.logger.Error("expected file", "error", err)
+		http.Error(writer, "expected file", http.StatusUnprocessableEntity)
 		return
 	}
 
-	err = mp.saveFile(strconv.Itoa(productId), fileHeader.Filename, imageFile)
+	err = handler.saveFile(strconv.Itoa(productId), fileHeader.Filename, imageFile)
 	if err != nil {
-		mp.log.Error("unable to save file", "error", err)
-		http.Error(rw, "unable to save file", http.StatusInternalServerError)
+		handler.logger.Error("unable to save file", "error", err)
+		http.Error(writer, "unable to save file", http.StatusInternalServerError)
 		return
 	}
 
 	if id == "" {
-		err = mp.srv.AddProduct(&product)
+		err = handler.productService.AddProduct(&product)
 	} else {
-		err = mp.srv.UpdateProduct(&product)
+		err = handler.productService.UpdateProduct(&product)
 	}
 
 	if err != nil {
-		mp.log.Error("request to gRPC service", "error", err)
-		http.Error(rw, "request to gRPC service", http.StatusBadRequest)
+		handler.logger.Error("request to gRPC service", "error", err)
+		http.Error(writer, "request to gRPC service", http.StatusBadRequest)
 	}
 }
 
-func (mp *MultipartHandler) saveFile(id, path string, r io.ReadCloser) error {
+func (handler *MultipartHandler) saveFile(id, path string, readCloser io.ReadCloser) error {
 	filePath := filepath.Join(id, path)
 
-	_, err := mp.stg.Save(filePath, r)
+	_, err := handler.storage.Save(filePath, readCloser)
 	if err != nil {
-		mp.log.Info("file from multipart/form-data has been successfully uploaded to", "filePath", filePath)
+		handler.logger.Info("file from multipart/form-data has been successfully uploaded to", "filePath", filePath)
 	}
 
 	return err

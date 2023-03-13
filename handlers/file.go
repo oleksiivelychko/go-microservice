fcace87290076f026a964b838ca079c786554f4c
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-13 08:36:54 +0200

    use own utils package

--- handlers/file.go
+++ handlers/file.go
@@ -3,40 +3,40 @@ package handlers
 import (
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/go-hclog"
-	"github.com/oleksiivelychko/go-microservice/contracts"
+	"github.com/oleksiivelychko/go-utils/storage"
 	"net/http"
 	"path/filepath"
 )
 
 type File struct {
 	logger  hclog.Logger
-	storage contracts.Storage
+	storage storage.LocalStorage
 }
 
-func NewFileHandler(storage contracts.Storage, logger hclog.Logger) *File {
+func NewFileHandler(storage storage.LocalStorage, logger hclog.Logger) *File {
 	return &File{storage: storage, logger: logger}
 }
 
-func (file *File) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (file *File) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 	// mux already has checked parameters according to regex rules
-	vars := mux.Vars(request)
-	productId := vars["id"]
-	filename := vars["filename"]
+	muxVars := mux.Vars(request)
+	productId := muxVars["id"]
+	filename := muxVars["filename"]
 
-	file.saveFile(productId, filename, writer, request)
+	file.saveFile(productId, filename, responseWriter, request)
 }
 
-func (file *File) invalidURI(uri string, writer http.ResponseWriter) {
+func (file *File) invalidURI(uri string, responseWriter http.ResponseWriter) {
 	file.logger.Error("invalid path", "path", uri)
-	http.Error(writer, "invalid file path: should be in the format: /[id]/[filename]", http.StatusBadRequest)
+	http.Error(responseWriter, "invalid file path: should be in the format: /[id]/[filename]", http.StatusBadRequest)
 }
 
-func (file *File) saveFile(id, filename string, writer http.ResponseWriter, request *http.Request) {
+func (file *File) saveFile(id, filename string, responseWriter http.ResponseWriter, request *http.Request) {
 	filePath := filepath.Join(id, filename)
 	_, err := file.storage.Save(filePath, request.Body)
 	if err != nil {
 		file.logger.Error("unable to save file", "error", err)
-		http.Error(writer, "unable to save file", http.StatusInternalServerError)
+		http.Error(responseWriter, "unable to save file", http.StatusInternalServerError)
 	}
 
 	file.logger.Info("file has been successfully uploaded to", "filePath", filePath)

3b9cb351c4ac7aa81f841669be6f268bbbcb9209
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-25 10:36:56 +0200

    refactoring product handler: stick to naming convention

--- product_handler/handler.go
+++ product_handler/handler.go
@@ -1,56 +1,35 @@
-package handlers
+package product_handler
 
 import (
 	"github.com/gorilla/mux"
 	"github.com/hashicorp/go-hclog"
 	"github.com/oleksiivelychko/go-microservice/service"
-	validatorUtils "github.com/oleksiivelychko/go-utils/validator_helper"
+	"github.com/oleksiivelychko/go-utils/validator_helper"
 	"net/http"
 	"strconv"
 )
 
-// ProductHandler for CRUD actions regarding api.Product objects.
-type ProductHandler struct {
-	logger         hclog.Logger
-	validation     *validatorUtils.Validation
-	productService *service.ProductService
-}
-
 // KeyProduct is a key used for the api.Product object in the context.
 type KeyProduct struct{}
 
+// ProductHandler for CRUD actions regarding api.Product objects.
+type ProductHandler struct {
+	logger         hclog.Logger
+	validation     *validator_helper.Validation
+	productService *service.ProductService
+}
+
 func NewProductHandler(
 	logger hclog.Logger,
-	validation *validatorUtils.Validation,
+	validation *validator_helper.Validation,
 	productService *service.ProductService,
 ) *ProductHandler {
 	return &ProductHandler{logger, validation, productService}
 }
 
-// GenericError is a generic error message returned by a server.
-type GenericError struct {
-	Message string `json:"message"`
-}
-
-// ValidationErrors is a collection of validation error messages.
-type ValidationErrors struct {
-	Messages []string `json:"messages"`
-}
-
-type NotFound struct {
-	Message string `json:"message"`
-}
-
-// GrpcError means that request to gRPC service failed.
-type GrpcError struct {
-	Message string `json:"message"`
-}
-
 // getProductID returns ID parameter from URL.
 func (productHandler *ProductHandler) getProductID(r *http.Request) int {
-	// parse the product id from the url
 	muxVars := mux.Vars(r)
-	// convert the id into an integer and return
 	id, err := strconv.Atoi(muxVars["id"])
 	if err != nil {
 		// should never happen as the router ensures that this is a valid number

4d26dd23dba3a0106336d95a0eeccfbfab77d13f
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-25 10:54:38 +0200

    refactoring validation util: stick to naming convention

--- utils/validation.go
+++ utils/validation.go
@@ -2,14 +2,14 @@ package utils
 
 import (
 	"github.com/go-playground/validator/v10"
-	validatorUtils "github.com/oleksiivelychko/go-utils/validator_helper"
+	"github.com/oleksiivelychko/go-utils/validator_helper"
 	"regexp"
 )
 
-func NewValidation() *validatorUtils.Validation {
+func NewValidation() *validator_helper.Validation {
 	validate := validator.New()
 	_ = validate.RegisterValidation("sku", validateSKU)
-	return &validatorUtils.Validation{ValidatorValidate: validate}
+	return &validator_helper.Validation{ValidatorValidate: validate}
 }
 
 func validateSKU(fl validator.FieldLevel) bool {

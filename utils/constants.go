05cd22c143dcdbc0bea49091a43bbf182429a6d7
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-25 15:39:28 +0200

    define constants

---
+++ utils/constants.go
@@ -0,0 +1,16 @@
+package utils
+
+const CurrencyQueryParam = "currency"
+const CurrencyRegex = "{[A-Z]{3}}"
+const DefaultCurrency = "USD"
+const LocalDataPath = "./data/products.json"
+const LocalStorageBasePath = "/files/"
+const LocalStoragePath = "./public" + LocalStorageBasePath
+const MaxFileSize5MB = 1024 * 1000 * 5
+const ProductFileURL = "{id:[0-9]+}/{filename:[a-zA-Z]+\\.(?:png|jpe?g)}"
+const ProductURL = "/products/{id:[0-9]+}"
+const ProductsFormURL = "/products-form"
+const ProductsURL = "/products"
+const RedocURL = "/redoc"
+const SwaggerURL = "/swagger"
+const SwaggerYAML = "/sdk/swagger.yaml"

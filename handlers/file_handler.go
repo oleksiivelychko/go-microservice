9b736f5278fbc7b251ddf049a79e15c493a80224
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-03-25 10:23:12 +0200

    remove invalidURI method

---
+++ handlers/file_handler.go
@@ -0,0 +1,32 @@
+package handlers
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-hclog"
+	"github.com/oleksiivelychko/go-utils/local_storage"
+	"net/http"
+	"path/filepath"
+)
+
+type FileHandler struct {
+	logger  hclog.Logger
+	storage local_storage.ILocalStorage
+}
+
+func NewFileHandler(storage local_storage.ILocalStorage, logger hclog.Logger) *FileHandler {
+	return &FileHandler{storage: storage, logger: logger}
+}
+
+func (fileHandler *FileHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	// mux already has checked parameters according to regex rules
+	muxVars := mux.Vars(request)
+	filePath := filepath.Join(muxVars["id"], muxVars["filename"])
+
+	_, err := fileHandler.storage.Save(filePath, request.Body)
+	if err != nil {
+		fileHandler.logger.Error("unable to save file", "error", err)
+		http.Error(responseWriter, "unable to save file", http.StatusInternalServerError)
+	}
+
+	fileHandler.logger.Info("file has been successfully uploaded to", "filePath", filePath)
+}

677e57e4cfd0257021f0e5f05d130504a5e28d3b
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-04-15 14:41:25 +0300

    define default values

--- env/default.go
+++ env/default.go
@@ -1,15 +1,25 @@
-package utils
+package env
 
-const CurrencyQueryParam = "currency"
-const CurrencyRegex = "{[A-Z]{3}}"
+import (
+	"fmt"
+	"os"
+)
+
 const DefaultCurrency = "USD"
 const FormDataMaxMemory32MB = 128 * 1024
-const LocalDataPath = "./data/products.json"
+const LocalDataPath = "./public/data/products.json"
 const LocalStorageBasePath = "/files/"
 const LocalStoragePath = "./public" + LocalStorageBasePath
 const MaxFileSize5MB = 1024 * 1000 * 5
 const ProductFileURL = "{id:[0-9]+}/{filename:[a-zA-Z]+\\.(?:png|jpe?g)}"
-const ProductURL = "/products/{id:[0-9]+}"
 const RedocURL = "/redoc"
 const SwaggerURL = "/swagger"
 const SwaggerYAML = "/sdk/swagger.yaml"
+
+func ServerAddr() (string, string) {
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	portGRPC := os.Getenv("PORT_GRPC")
+
+	return fmt.Sprintf("%s:%s", host, port), fmt.Sprintf("%s:%s", host, portGRPC)
+}

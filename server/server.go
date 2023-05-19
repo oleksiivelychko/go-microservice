f2c5b704c86dc1145680f26055d542b3f8c79e3d
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-05-19 20:59:44 +0300

    use own error logger

--- server/server.go
+++ server/server.go
@@ -1,19 +1,19 @@
 package server
 
 import (
-	"github.com/hashicorp/go-hclog"
+	"log"
 	"net/http"
 	"time"
 )
 
-func NewHTTP(addr string, handler http.Handler, errorLog hclog.Logger) *http.Server {
+func NewHTTP(addr string, handler http.Handler, errorLog *log.Logger) *http.Server {
 	server := &http.Server{
 		Addr:         addr,
 		Handler:      handler,
 		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
 		ReadTimeout:  10 * time.Second,  // max time to read request from the client
 		WriteTimeout: 10 * time.Second,  // max time to write response to the client
-		ErrorLog:     errorLog.StandardLogger(&hclog.StandardLoggerOptions{InferLevels: true}),
+		ErrorLog:     errorLog,
 	}
 
 	return server

0af3d0ee9f66f6b96fdf238c1f6b4a91b4745f45
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-05-19 08:48:10 +0300

    update error message

--- data/loader_test.go
+++ data/loader_test.go
@@ -7,6 +7,6 @@ import (
 func TestData_LoadProductsFromJSON(t *testing.T) {
 	products := LoadProductsFromJSON("./../public/data/products.json")
 	if len(products) == 0 {
-		t.Error("unable to load, list is empty")
+		t.Error("products list is empty")
 	}
 }
